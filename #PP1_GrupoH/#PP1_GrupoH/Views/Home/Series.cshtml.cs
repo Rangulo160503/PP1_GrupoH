b3c469318c342a419ed1f55b26a4879c9c04cfb6
CaChaves <chavescatalina712@gmail.com>
2024-06-04 22:02:45 -0600

    Primer commit

---
+++ #PP1_GrupoH/#PP1_GrupoH/Views/Home/Series.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _PP1_GrupoH.Views.Home
+{
+    public class SeriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ #PP1_GrupoH/#PP1_GrupoH/Views/Home/Songs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _PP1_GrupoH.Views.Home
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
